fde4fdd95c78cc1dae454260b4ea622f5b2e96a2
Yordy Martinez <ymartinez@cincinnatus.edu.do>
2024-06-30 22:21:49 -0400

    fix variables names and clean the code

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const game = document.querySelector(".grid__game");
-const gridSquare = document.querySelectorAll(".game__box");
+const gameBoard = document.querySelector(".grid__game");
+const gameSquare = document.querySelectorAll(".game__box");
 const playerSpan = document.querySelector(".player");
 const winnerText = document.querySelector(".winner");
 const btnReset = document.querySelector(".btn__reset");
-let Square = Array(9).fill(null);
+
+let arrayBoard = Array(9).fill(null);
 let player = true;
 let gameFinished = false;
-let squaresX = [];
 
 const WINNER_COMBINATIONS = [
   [0, 1, 2],
@@ -23,18 +23,37 @@ document.addEventListener("DOMContentLoaded", (e) => {
   playerSpan.textContent = player ? "X" : "O";
 });
 
-const changePlayer = () => {
-  // player ? (player = false) : (player = true);
-  player = !player;
-  playerSpan.textContent = player ? "X" : "O";
+gameSquare.forEach((square, index) => {
+  square.addEventListener("click", (e) => {
+    handleSquareClick(square, index);
+  });
+});
+
+const handleSquareClick = (square, index) => {
+  if (arrayBoard[index] != null || gameFinished) return;
+
+  const playerGame = player ? "X" : "O";
+  arrayBoard[index] = playerGame;
+  square.textContent = arrayBoard[index];
+
+  const winner = winnerPlayer(arrayBoard);
+
+  if (winner) {
+    winnerText.textContent = "El ganador es: " + winner;
+    gameFinished = true;
+    return;
+  } else if (checkDraw(arrayBoard)) {
+    gameFinished = false;
+    winnerText.textContent = "EMPATE";
+    return;
+  }
+
+  changePlayer();
 };
 
-const emptyGame = (boardToCheck) => {
-  for (const combo of WINNER_COMBINATIONS) {
-    const [a, b, c] = combo;
-    if (boardToCheck[a] !== boardToCheck[b] && boardToCheck)
-      return console.log("Empate");
-  }
+const changePlayer = () => {
+  player = !player;
+  playerSpan.textContent = player ? "X" : "O";
 };
 
 const winnerPlayer = (boardToCheck) => {
@@ -45,8 +64,6 @@ const winnerPlayer = (boardToCheck) => {
       boardToCheck[a] === boardToCheck[b] &&
       boardToCheck[a] === boardToCheck[c]
     ) {
-      // winnerText.textContent = boardToCheck[a];
-
       return boardToCheck[a];
     }
   }
@@ -54,43 +71,19 @@ const winnerPlayer = (boardToCheck) => {
   return null;
 };
 
-const resetGame = () => {
-  Square = Array(9).fill(null);
-  gameFinished = false;
-  winnerText.textContent = "";
-
-  gridSquare.forEach((square, index) => {
-    square.textContent = "";
-  });
-};
-
 const checkDraw = (board) => {
   return board.every((cell) => cell !== null) && winnerPlayer(board) === null;
 };
 
-gridSquare.forEach((square, index) => {
-  square.addEventListener("click", (e) => {
-    if (Square[index] != null || gameFinished) return;
+const resetGame = () => {
+  arrayBoard = Array(9).fill(null);
+  gameFinished = false;
+  winnerText.textContent = "";
 
-    const playerGame = player ? "X" : "O";
-    Square[index] = playerGame;
-    square.textContent = Square[index];
-
-    const winner = winnerPlayer(Square);
-
-    if (winner) {
-      winnerText.textContent = "El ganador es: " + winner;
-      gameFinished = true;
-      return;
-    } else if (checkDraw(Square)) {
-      gameFinished = false;
-      winnerText.textContent = "EMPATE";
-      return;
-    }
-
-    changePlayer();
+  gameSquare.forEach((square, index) => {
+    square.textContent = "";
   });
-});
+};
 
 btnReset.addEventListener("click", (e) => {
   resetGame();
